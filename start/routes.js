6352c3c354830c5deb8bfbe38a7e321acd9febf8
quaresmateo <teoian.giovane@gmail.com>
2020-08-26 17:38:33 -0300

    Modifica rota

--- start/routes.js
+++ start/routes.js
@@ -18,7 +18,7 @@ const Route = use('Route')
 
 Route.group(() => {
   Route.post('/usuario', 'UserController.create')
-  Route.get('/usuarios', 'UserController.').middleware('auth:jwt')
+  Route.get('/usuarios', 'UserController.').middleware('auth')
 
   Route.put('/usuario', 'UserController.update').middleware('auth')
 }).prefix('api/v1')
