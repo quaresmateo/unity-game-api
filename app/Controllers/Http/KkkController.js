e56431f057cd20a9bd98a89470eff0af5bb3b409
quaresmateo <teoian.giovane@gmail.com>
2020-08-27 16:19:07 -0300

    Corrige erro de sintaxe

---
+++ app/Controllers/Http/KkkController.js
@@ -0,0 +1,86 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with kkks
+ */
+class KkkController {
+  /**
+   * Show a list of all kkks.
+   * GET kkks
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {}
+
+  /**
+   * Render a form to be used for creating a new kkk.
+   * GET kkks/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create({ request, response, view }) {}
+
+  /**
+   * Create/save a new kkk.
+   * POST kkks
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {}
+
+  /**
+   * Display a single kkk.
+   * GET kkks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {}
+
+  /**
+   * Render a form to update an existing kkk.
+   * GET kkks/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit({ params, request, response, view }) {}
+
+  /**
+   * Update kkk details.
+   * PUT or PATCH kkks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {}
+
+  /**
+   * Delete a kkk with id.
+   * DELETE kkks/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) {}
+}
+
+module.exports = KkkController
