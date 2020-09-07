198caa494bd825cf6e89059078b7d9d88e11e90c
quaresmateo <teoian.giovane@gmail.com>
2020-09-07 15:48:23 -0300

    Relaciona tabelas

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -7,13 +7,18 @@ class UserSchema extends Schema {
   up() {
     this.create('users', (table) => {
       table.increments()
-      table.integer('institution_id', 80).notNullable().unique()
+      table.integer('institution_id', 80).notNullable().unsigned()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
       table.string('fullname', 120).notNullable()
       table.string('profession', 120).notNullable()
       table.timestamps()
+
+      table
+        .foreign('institution_id')
+        .references('institutions.id')
+        .onDelete('cascade')
     })
   }
 
