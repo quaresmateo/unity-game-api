0280de1d913381131ef3d3ca9d21d49d1e0ff077
quaresmateo <teoian.giovane@gmail.com>
2020-09-07 15:37:30 -0300

    Modifica campos de tabela

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -7,10 +7,12 @@ class UserSchema extends Schema {
   up() {
     this.create('users', (table) => {
       table.increments()
+      table.integer('institution_id', 80).notNullable().unique()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
-      table.enum('type', ['tutor', 'aluno']).notNullable()
+      table.string('fullname', 120).notNullable()
+      table.string('profession', 120).notNullable()
       table.timestamps()
     })
   }
