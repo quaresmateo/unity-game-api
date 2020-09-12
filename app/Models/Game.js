6d6da25ae73f2ae68d6daa3da5b6cf5024524f3b
quaresmateo <teoian.giovane@gmail.com>
2020-09-12 12:32:40 -0300

    Adiciona relação de tabelas em model

--- app/Models/Game.js
+++ app/Models/Game.js
@@ -7,6 +7,10 @@ class Game extends Model {
   category() {
     return this.hasOne('App/Models/Category')
   }
+
+  playerSetting() {
+    return this.hasOne('App/Models/QuebraCabecaPlayerSetting')
+  }
 }
 
 module.exports = Game
