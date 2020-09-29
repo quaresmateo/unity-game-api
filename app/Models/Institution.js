e99065f137b4a351bf9572561bec0e32662389d0
quaresmateo <teoian.giovane@gmail.com>
2020-09-29 15:23:23 -0300

    Adiciona quem criou insti. em model Institution

--- app/Models/Institution.js
+++ app/Models/Institution.js
@@ -3,6 +3,10 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class Institution extends Model {}
+class Institution extends Model {
+  whoCreated() {
+    return this.hasOne('App/Models/User')
+  }
+}
 
 module.exports = Institution
