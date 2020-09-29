ab095a90a3301bf73b38597507196d7329d4a940
quaresmateo <teoian.giovane@gmail.com>
2020-09-29 15:18:43 -0300

    Formata model

--- app/Models/Institution.js
+++ app/Models/Institution.js
@@ -3,7 +3,6 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class Institution extends Model {
-}
+class Institution extends Model {}
 
 module.exports = Institution
