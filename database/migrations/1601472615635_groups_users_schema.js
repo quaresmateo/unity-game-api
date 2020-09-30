caa6a1357726b4f256124c042188f616b85eb363
quaresmateo <teoian.giovane@gmail.com>
2020-09-30 10:45:36 -0300

    Adiciona campos em tabela `groups_users`

--- database/migrations/1601472615635_groups_users_schema.js
+++ database/migrations/1601472615635_groups_users_schema.js
@@ -4,14 +4,19 @@
 const Schema = use('Schema')
 
 class GroupsUsersSchema extends Schema {
-  up () {
+  up() {
     this.create('groups_users', (table) => {
       table.increments()
+      table.integer('user_id').unsigned()
+      table.integer('group_id').unsigned()
       table.timestamps()
+
+      table.foreign('user_id').references('users.id').onDelete('cascade')
+      table.foreign('group_id').references('groups.id').onDelete('cascade')
     })
   }
 
-  down () {
+  down() {
     this.drop('groups_users')
   }
 }
