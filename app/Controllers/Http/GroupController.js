3cd8ddbfb6f95344be7634bd16782673dcebbdd1
quaresmateo <teoian.giovane@gmail.com>
2020-10-09 19:29:16 -0300

    Cria parcialmente método post em concontroller de grupo

--- app/Controllers/Http/GroupController.js
+++ app/Controllers/Http/GroupController.js
@@ -4,90 +4,38 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
-/**
- * Resourceful controller for interacting with groups
- */
+const Group = use('App/Models/Group')
+const Player = use('App/Models/Player')
+
 class GroupController {
-  /**
-   * Show a list of all groups.
-   * GET groups
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
+  async index({ request, response, view }) {}
+
+  async create({ request, response, view }) {}
+
+  async store({ request, response, auth }) {
+    const user_id = auth.user.id
+    const { name, players_ids } = request.all()
+    const group = await Group.create({ name, user_id })
+
+    //está bugado: solução fazer query direto
+    players_ids.forEach(async (player_id) => {
+      const player = await Player().findOrFail(player_id)
+      await group.players().attach([player.id])
+    })
+
+    return response.json({
+      data: group,
+      message: 'Grupo criado'
+    })
   }
 
-  /**
-   * Render a form to be used for creating a new group.
-   * GET groups/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
+  async show({ params, request, response, view }) {}
 
-  /**
-   * Create/save a new group.
-   * POST groups
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
-  }
+  async edit({ params, request, response, view }) {}
 
-  /**
-   * Display a single group.
-   * GET groups/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
-  }
+  async update({ params, request, response }) {}
 
-  /**
-   * Render a form to update an existing group.
-   * GET groups/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update group details.
-   * PUT or PATCH groups/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a group with id.
-   * DELETE groups/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
-  }
+  async destroy({ params, request, response }) {}
 }
 
 module.exports = GroupController
