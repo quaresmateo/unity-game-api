032bf75316c10a988354a2a70cbce72ee67f5576
quaresmateo <teoian.giovane@gmail.com>
2020-10-09 19:28:43 -0300

    Adiciona relacionamento em model de jogador

--- app/Models/Player.js
+++ app/Models/Player.js
@@ -3,6 +3,10 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class Player extends Model {}
+class Player extends Model {
+  groups() {
+    return this.belongsToMany('App/Models/Group').pivotTable('groups_players')
+  }
+}
 
 module.exports = Player
