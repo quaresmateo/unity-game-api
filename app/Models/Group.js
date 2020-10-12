527a9c092be6f2e3df96855fe66b9abf8a41aab2
quaresmateo <teoian.giovane@gmail.com>
2020-10-12 12:16:35 -0300

    Cria relacionamento em models

--- app/Models/Group.js
+++ app/Models/Group.js
@@ -7,6 +7,10 @@ class Group extends Model {
   players() {
     return this.belongsToMany('App/Models/Player').pivotTable('groups_players')
   }
+
+  themes() {
+    return this.belongsToMany('App/Models/Theme').pivotTable('groups_themes')
+  }
 }
 
 module.exports = Group
