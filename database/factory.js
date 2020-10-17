3ba129d5dfc6bf58ead98a3cdd9e5f1fded92d4b
quaresmateo <teoian.giovane@gmail.com>
2020-10-17 16:29:30 -0300

    Cria seed de jogadores e corrige param. de funções

--- database/factory.js
+++ database/factory.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const { get } = require('@adonisjs/lucid/src/Factory')
+
 /*
 |--------------------------------------------------------------------------
 | Factory
@@ -20,12 +22,17 @@ const Factory = use('Factory')
 //   }
 // })
 
-function getRandomInt(min, max) {
+/**
+ * Retorna números inteiros aleatórios de um interválo
+ *
+ * Padrão de 1 até 5
+ */
+function getRandomInt(min = 1, max = 5) {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
 Factory.blueprint('App/Models/Institution', (faker) => {
-  const user_id = getRandomInt(1, 5)
+  const user_id = getRandomInt()
   return {
     name: faker.word().toUpperCase(),
     user_id
@@ -43,7 +50,7 @@ Factory.blueprint('App/Models/User', (faker) => {
       .split(' ')
       .join(faker.string({ length: 1, pool: '_.' })) + '@email.com'
   const userType = ['responsible', 'professional']
-  const role = userType[getRandomInt(1, 2)]
+  const role = userType[getRandomInt(0, 1)]
 
   return {
     fullname,
@@ -51,7 +58,37 @@ Factory.blueprint('App/Models/User', (faker) => {
     email,
     role,
     profession: faker.word(),
-    institution_id: getRandomInt(1, 5),
+    institution_id: getRandomInt(),
     password: '123456'
+  }
+})
+
+Factory.blueprint('App/Models/Player', (faker) => {
+  const fullname = faker.name({ middle: true, nationality: 'it' })
+  const username =
+    fullname.split(' ')[0].toLowerCase() +
+    faker.string({ length: 3, pool: '_123456789' })
+  const date_of_birth = `${getRandomInt(1, 27)}-${getRandomInt(
+    1,
+    12
+  )}-${getRandomInt(2000, 2018)}`
+  const koh_types = [
+    'visual',
+    'física',
+    'intelectual',
+    'mental',
+    'auditiva',
+    'múltipla'
+  ]
+  const kind_of_handicap = koh_types[getRandomInt(0, 6)]
+
+  return {
+    user_id: getRandomInt(),
+    fullname,
+    username,
+    identification: faker.string({ length: 6, pool: '123456789' }),
+    date_of_birth,
+    kind_of_handicap,
+    diagnosis: faker.paragraph({ setence: 3 })
   }
 })
