f41e79d5a7024732278f74fdecce53a6d8dfd64b
quaresmateo <teoian.giovane@gmail.com>
2020-10-20 23:02:55 -0300

    Remove console.log

--- app/Controllers/Http/ThemeController.js
+++ app/Controllers/Http/ThemeController.js
@@ -23,7 +23,6 @@ class ThemeController {
     const data = { ...request.only(['name']), user_id }
     const theme = await Theme.create(data)
     const { groups_id: groups } = request.all()
-    console.log(groups)
 
     groups.forEach(async (group_id) => {
       const group = await Group.findOrFail(group_id)
