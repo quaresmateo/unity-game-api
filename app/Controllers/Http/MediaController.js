2c365cc38b51c9c0dfd213c907e42b879f666e9f
quaresmateo <teoian.giovane@gmail.com>
2020-11-04 21:21:54 -0300

    Cria método para criar mídia

--- app/Controllers/Http/MediaController.js
+++ app/Controllers/Http/MediaController.js
@@ -3,13 +3,42 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
+const Helpers = use('Helpers')
+const Media = use('App/Models/Media')
 
 class MediaController {
   async index({ request, response }) {}
 
   async create({ request, response }) {}
 
-  async store({ request, response }) {}
+  async store({ params, request, response }) {
+    const { theme_id } = params
+    const { local_layout } = request.all()
+    const file = request.file('file', {
+      types: ['image', 'audio', 'video'],
+      size: '10mb'
+    })
+
+    const { type, clientName: name } = file
+    const src = `/assets/tema/${theme_id}/${type}/`
+
+    await file.move(Helpers.resourcesPath(src))
+
+    if (!profilePics.errors()) {
+      const media = await Media.create({
+        name,
+        type,
+        local_layout,
+        src,
+        theme_id
+      })
+
+      return response.json({
+        message: 'Criado',
+        data: media
+      })
+    }
+  }
 
   async show({ params, request, response }) {}
 
